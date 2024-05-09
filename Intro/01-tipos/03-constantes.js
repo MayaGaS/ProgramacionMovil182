f2d781682882d33fd3b6a9146ad890cdc7463345
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-09 08:55:24 -0600

    03-constantes

---
+++ Intro/01-tipos/03-constantes.js
@@ -0,0 +1,7 @@
+const nombre = "Hola Mundo";
+nombre = "Chachito feliz";
+
+console.log(nombre);
+
+// cuando se usa let cuando se vaya a cambiar la variable 
+// se usa const la mayoria de las veces 
