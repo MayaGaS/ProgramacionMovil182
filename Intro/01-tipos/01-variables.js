412bafcc7850c5cf1284f92cd5bf3ed09ef835c2
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-09 08:27:31 -0600

    01-variables

---
+++ Intro/01-tipos/01-variables.js
@@ -0,0 +1,15 @@
+//convensiones que se pueden utilizar 
+UpperCameCase, //significa que cuando se crea una variable, la primera letra de cada palabra que se utilice para nombrar la varible debe ser con mayúscula y así con cada una de las palabras que contenga la variable 
+camelCase, //la primera palabra es con minuscula y las que siguen es con mayúscula
+snake_case //cada palabra debe ser separada con un _
+
+let nombre = 'Hola Mundo'; //para definir la variable let + nombre + = + el valor que se le va a asignar
+let NombreCompleto;
+let nombreCompleto; //camelCase es la convension más utilizada
+let nombre_completo;
+console.log(NombreCompleto); //se imprime en la consola
+
+let apellido
+let animal;
+apellido = 'Schurmann'
+
