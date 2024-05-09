aa84a5b8fcc58fa24ac25058da0f187a32249b39
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-09 08:36:10 -0600

    02-primitivos

---
+++ Intro/01-tipos/02-primitivos.js
@@ -0,0 +1,16 @@
+// 1° tipo de dato primitivo NUMERO 
+let numero = 1; 
+
+// 2° tipo de dato primitivo STRING
+let texto = "Hola Mundo";
+
+// 3° tipo de dato BOOLEAN 
+let verdadero = true;
+let falso = false;
+
+// 4° tipo de dato UNDEFINED 
+let noDefinido;
+let undef = undefined;
+
+// 5° tipo de dato NULL 
+let nulo = null; 
