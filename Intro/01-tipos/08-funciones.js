486d5c6d828af2eadd0ef089bbb36ef6d16ae312
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-10 08:52:22 -0600

    08-funciones

---
+++ Intro/01-tipos/08-funciones.js
@@ -0,0 +1,18 @@
+/**
+ * LAS FUNCIONES SON BLOQUES FUNDAMENTALES DENTRO DE LA CONSTRUCCION DE APLICACIONES EN JS
+ * SON COMO SI FUERAN PROCEDIMIENTOS O UN LISTADO DE INSTRUCCIONES QUE SE DEBEN EJECUTAR
+ */
+
+function saludar() {
+    console.log('Hola Mundo');
+}
+
+// llamar a una funcion 
+saludar();
+
+function suma() {
+    return 2 + 2;
+}
+
+//let resultado = suma();
+console.log(suma());
