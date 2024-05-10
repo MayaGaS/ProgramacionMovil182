4924fe280f77b20f793f4093ba43d6f8b752d508
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-10 14:29:50 -0600

    08-for-in

---
+++ Intro/03-control-flujo/08-for-in.js
@@ -0,0 +1,15 @@
+// nos sirve para que podamos iterar las propiedades de un objeto
+
+let user = {
+    id: 1,
+    name: 'Chanchito feliz',
+    age: 25,
+};
+for (let prop in user){
+    console.log(prop, user[prop]);
+}
+
+let animales = ['Chanchito feliz', 'Dragon', 'Canguro'];
+for (let indice in animales) {
+    console.log(indice, animales[indice]);
+}
