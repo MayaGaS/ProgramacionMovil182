079b762f3dd444588346bb6fb6cec64e8e811ed2
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-10 12:58:30 -0600

    08-ternarios

---
+++ Intro/02-operadores/08-ternario.js
@@ -0,0 +1,7 @@
+// expresion ? 'si es true' : 'si es false'
+// el operador ternario cumple con una funcion muy similar al operador de control de flujo if
+
+let edad = 25;
+let acceso = edad > 17 ? 'Permitir ingreso' : 'No puede ingresar';
+
+console.log(acceso);
