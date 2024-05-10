0f68fb547574e77d218421cd5dd593f135528c44
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-10 14:36:10 -0600

    09-break-continue

---
+++ Intro/03-control-flujo/09-break-continue.js
@@ -0,0 +1,15 @@
+// break nos permite salirnos del loop 
+// continue nos permite saltarnos una iteracion 
+// se puede utilizar para for, for in, for of, do while
+
+let i = 0;
+while (i < 6) {
+    i++;
+    if (i == 2) {
+        continue;
+    }
+    if (i == 4) {
+        break;
+    }
+    console.log(i);
+}
