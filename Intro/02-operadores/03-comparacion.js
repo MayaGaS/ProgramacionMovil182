9bc66e743ecf11fecd4cc7c2536d5da1e4b1510b
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-10 11:40:30 -0600

    03-comparacion

---
+++ Intro/02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+
+// RELACIONALES 
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+// IGUALDAD 
+console.log(a == 10);
+console.log(a != 10);
+console.log(a === '10');
+console.log(a !== '10');
