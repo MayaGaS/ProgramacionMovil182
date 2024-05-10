328d4950ce63c8e28a4eaafa13bd2b2bc4c90142
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-10 08:09:34 -0600

    05-comentarios

---
+++ Intro/01-tipos/05-comentarios.js
@@ -0,0 +1,17 @@
+//njdkjnfalknd
+
+/*
+Esta es la amnera 
+oficial para 
+escribir comentarios
+*/
+
+/**
+ * Este es un comentario 
+ * de muchas
+ * lineas
+ */
+
+let numero = 42; // esre es otro comentario
+
+console.log(numero);
