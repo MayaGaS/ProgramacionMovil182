012e8fe6d83a4ce9c5dd4ebb59123d0f960b32ae
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-10 14:10:55 -0600

    05-do-while

---
+++ Intro/03-control-flujo/05-do-while.js
@@ -0,0 +1,18 @@
+// cuales son los numeros pares 
+
+// while se evalua primero la condicion  
+let i = 2;
+while (i < 2) {
+    if (i % 2 == 0) {
+        console.log('Numero par', i);
+    }
+    i++;
+}
+
+// do while ejecuta el codigo y luega realiza la evaluacion de la condicion
+// do {
+//     if (i % 2 == 0) {
+//         console.log('Numero par', i);
+//     }
+//     i++;
+// } while (i < 2);
