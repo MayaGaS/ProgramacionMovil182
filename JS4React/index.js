9672e60f7795efdfc58dff36c691df3345d265e4
MayaGaS <11mayagarrido.11@gmail.com>
2024-05-20 15:40:49 -0600

    Avance pract2

---
+++ JS4React/index.js
@@ -0,0 +1,29 @@
+// Funcion
+// function suma(a, b) {
+//     return a + b;
+// } 
+
+// Funcion farArrow
+// const suma = (a,b) => {
+//     return a + b;
+// }
+
+// Funcion farArrow simplificada
+// const suma = (a,b) => a+b
+
+console.log(suma(2,3))
+
+// Objeto
+const usuario = {
+    nombre: 'Ivan',
+    apellido: 'Guerra',
+    edad: 36,
+    direccion: {
+        pais: 'Mexico',
+        ciudad: 'Queretaro',
+        calle: 'La misma 123'
+    },
+    amigos: ['Alex', 'Carlos'],
+    estatus: true,
+    enviaCorreo: enviarCorr =() => 'enviando...'
+}
