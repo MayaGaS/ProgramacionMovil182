56b0046090adaac86e7cc6a31cde005600871b27
MayaGaS <11mayagarrido.11@gmail.com>
2024-06-18 11:34:19 -0600

    Practica 8

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,65 +1,99 @@
 // Importaciones 
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import React,{useState} from 'react'; //Importacion para hacer usos de datos
 
 
 
 //Funciones 
-const Texto=({estilo})=>{ // Parametros en este caso estilos que estilo
+/* const Texto=({estilo})=>{ // Parametros en este caso estilos que estilo
   const[contenido, setContenido] = useState('holaMundo') // Variable de contenido (propiedad children), se u
   const actualizarContenido=()=>{ // Array function que va a permitir hacer esa actualizacion
     setContenido('State actualizó este texto')} 
   return(<Text style={[styles.text,estilo]} onPress={actualizarContenido}>{contenido}</Text>) // Mandar llamar el evento 
   // con 'style' especificamos la copnexión que está en styles, y colocamos el estilo de texto y estilo
-}
-
-
+} */
 
 //Existen componentes, area donde se va a ejecutar
 export default function App() {
+
+  const[text, setText]= useState('valor default')
+  const[submit, setSubmit]= useState('')
+
   return (
 
     <View style={styles.container}>
-      
-      <Texto estilo={styles.red}/>  {/* Exportamos estilo más especifico con el nombre que le pusimos (red, green, blue) */}
-      <Texto estilo={styles.green}/>
-      <Texto estilo={styles.blue}/>
+
+      <Text> Componente TextInput: {submit} </Text>
+      <TextInput style={styles.TextInput} placeholder='Escribe texto ...' onChangeText={(t)=>setText(t) } value={text}/> 
+      <Button title='Presioname...' onPress={()=>{setSubmit(text); alert('Texto enviado')}}/>
+      {/* <TouchableHighlight 
+        style={styles.touchable} 
+        onPress={() => {setSubmit(text); alert('TouchableHighlight presionado'); }}
+        underlayColor={"#DDDDDD"}
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}> TouchableHighlight Button </Text>
+        </View>
+        </TouchableHighlight> */}
+
+        {/* <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => {setSubmit(text); alert('TouchableOpacity presionado'); }}
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}> TouchableOpacity Button </Text>
+          </View>
+        </TouchableOpacity> */}
+
+      <TouchableWithoutFeedback
+        onPress={() => { setSubmit(text); alert('TouchableWithoutFeedback presionado'); }}
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>TouchableWithoutFeedback Button</Text>
+        </View>
+      </TouchableWithoutFeedback>
+
 
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
-
-// Configuraciones de hijas de estilos
+// Configuraciones de hojas de estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column', //La forma en la que se acomoda el texto ("orden")
     backgroundColor: '#fff',
-    alignItems: 'stretch', // alinea el orden de izquierda a derecha
-    justifyContent: 'space-evenly', // orden de arriba a abajo
+    alignItems: 'center', // alinea el orden de izquierda a derecha
+    justifyContent: 'center', // orden de arriba a abajo
   },
-  text:{
-    color: 'white', //Color blanco
-    fontSize: 25, //Tamaño del texto
-    /* height: 100,  */  //Alto del texto
-    /* width: 150, */ //Ancho del texto
+
+  TextInput: {
+    width: '30%',
+    height: 40,
+    borderBottomColor: 'purple', // pone color a la línea de abajo 
+    borderBottomWidth: 2, // es el grosor de la línea
   },
-  red:{
-    /* flex: 1, */ //Adaptacion de la panatalla
-    backgroundColor: 'red', //Color del fondo
-  },
-  green:{
-    /* flex: 2, */
-    backgroundColor: 'green',
-  },
-  blue:{
-    /* flex: 3, */
+
+  touchable: {
+    marginTop: 20,
+    padding: 10,
     backgroundColor: 'blue',
   },
+
+  touchableO: {    
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#DDDDDD',
+  },
+
+  toucableW: {
+    padding: 10,
+    backgroundColor: '#2196F3',
+  },
+
 });
 
 
